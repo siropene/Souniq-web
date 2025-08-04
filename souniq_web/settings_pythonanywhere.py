11976a1e914c1be8fe0026eb1868f224d6f69ea7
aherrasf <alberto.herrasti.figueroa@gmail.com>
2025-08-04 20:03:21 +0200

    Acvtualizaciones

--- souniq_web/settings_pythonanywhere.py
+++ souniq_web/settings_pythonanywhere.py
@@ -27,11 +27,29 @@ DATABASES = {
 }
 
 # Para desarrollo local con SQLite
+# Database configuration
 if DEBUG:
+    # SQLite para desarrollo/testing
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    # MySQL para producción en PythonAnywhere
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'aherrasf$souniq_db',
+            'USER': 'aherrasf',
+            'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+            'HOST': 'aherrasf.mysql.pythonanywhere-services.com',
+            'PORT': '3306',
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'charset': 'utf8mb4',
+            },
         }
     }
 
