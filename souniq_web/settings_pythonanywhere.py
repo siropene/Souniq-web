34ecf7a6aa60c24776d0c539a0d95dd5874b8603
aherrasf <alberto.herrasti.figueroa@gmail.com>
2025-08-21 19:21:07 +0200

    Mostrar emails en el log

--- souniq_web/settings_pythonanywhere.py
+++ souniq_web/settings_pythonanywhere.py
@@ -162,3 +162,17 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Crispy Forms configuration
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Email Configuration for PythonAnywhere
+# PythonAnywhere free accounts don't support external SMTP
+# Use console backend for development/testing
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# For production with PythonAnywhere paid account, uncomment and configure:
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+# DEFAULT_FROM_EMAIL = 'SouniQ <souniq.demo@gmail.com>'
